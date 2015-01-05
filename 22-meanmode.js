abd0472a6e2947b6f15f8e3bc2ac574290c9e59d
Jeesun Kim <jeesunikim@gmail.com>
2015-01-05 17:53:27 -0500

    22 mean mode update

---
+++ 22-meanmode.js
@@ -0,0 +1,19 @@
+function MeanMode(arr) { 
+  //mode is the repeated ones
+  //mean is average
+  var largest=arr.pop();
+  var mean=0;
+  var average;
+  for(var i=0;i<arr.length;i++){ //find average;
+  if(arr[i]);
+  mean+=arr[i];
+  average=mean/arr.length;
+  }
+  if(mode === average){
+    return 1;
+  }else{return 0;}        
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+MeanMode(readline()); 
