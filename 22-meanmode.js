637934881624bfc9a3e92cdaa1c09dbd84a1ab17
Jeesun Kim <jeesunikim@gmail.com>
2015-01-06 09:57:01 -0500

    renamed coderbytes and Mean Mode added w/comments

--- 22-meanmode.js
+++ 22-meanmode.js
@@ -1,19 +1,34 @@
 function MeanMode(arr) { 
-  //mode is the repeated ones
-  //mean is average
-  var largest=arr.pop();
-  var mean=0;
-  var average;
-  for(var i=0;i<arr.length;i++){ //find average;
-  if(arr[i]);
-  mean+=arr[i];
-  average=mean/arr.length;
+var order=arr.sort(function(a,b){return a-b});
+var frequency={}; //finding mode stackoverflow: http://stackoverflow.com/questions/3783950/how-to-get-the-item-that-appear-the-most-time-in-an-array
+var mean=0; //It has to be zero
+var average;
+var max=0;
+var result;
+
+for(var j=0;j<arr.length;j++){ //find mode
+  frequency[order[j]]=(frequency[order[j]] || 0)+1; // increment frequency.
+  // console.log(frequency[order[j]]);
+  if(frequency[order[j]] > max) { // is this frequency > max so far ?
+  max = frequency[order[j]];  // update max.
+  result = order[j];          // update result.
   }
-  if(mode === average){
-    return 1;
-  }else{return 0;}        
 }
-   
+
+//the below works if the array for sure has repeated values.
+// var mode=order[Math.floor(order.length/2)];
+// console.log(mode);
+
+for(var i=0;i<arr.length;i++){ //find average
+  mean+=arr[i];
+  }
+  average=mean/arr.length;
+  // console.log(average);
+  if(result === average){
+    return 1;
+  }else{return 0;}
+}
+
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
 MeanMode(readline()); 
